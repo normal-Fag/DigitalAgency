ae69be2a0a54e84cb202dee97a513231edc47fab
normal-Fag <72517164+normal-Fag@users.noreply.github.com>
2021-10-19 20:18:59 +0700

    Complete targets section

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ burger_button.addEventListener('click', () => {
 
 	let menu_bar = document.querySelector('.menu');
 	let main = document.querySelector('.main');
+	let body = document.querySelector('body');
 
+	body.classList.toggle('lock');
 	main.classList.toggle('active');
 	menu_bar.classList.toggle('active');
 	burger_button.classList.toggle('active');
