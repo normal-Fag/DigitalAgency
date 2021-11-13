f5a91547ad316a3ed842850001733f76287cb98e
normal-Fag <72517164+normal-Fag@users.noreply.github.com>
2021-11-14 02:29:36 +0700

    Complete advertising section

--- js/app.js
+++ js/app.js
@@ -1,14 +1,58 @@
-const burger_button = document.querySelector('.header__burger');
+window.addEventListener('load', init);
 
-burger_button.addEventListener('click', () => {
+let advertising_items
 
-	let menu_bar = document.querySelector('.menu');
-	let main = document.querySelector('.main');
-	let body = document.querySelector('body');
+function init() {
+	// Обработка advertising__item
+	advertising_items = document.querySelectorAll('.advertising__item');
+	setHeights();
+	wireUpTriggers();
+	window.addEventListener("resize", setHeights);
 
-	body.classList.toggle('lock');
-	main.classList.toggle('active');
-	menu_bar.classList.toggle('active');
-	burger_button.classList.toggle('active');
+	// Обработка "бургера" 
+	const burger_button = document.querySelector('.header__burger');
+	const menu_bar = document.querySelector('.menu');
+	const main = document.querySelector('.main');
+	const body = document.querySelector('body');
 
-})
+	burger_button.addEventListener('click', () => {
+
+		body.classList.toggle('lock');
+		main.classList.toggle('active');
+		menu_bar.classList.toggle('active');
+		burger_button.classList.toggle('active');
+
+	})
+}
+
+function setHeights() {
+	advertising_items.forEach(advertising_item => {
+		// Get content
+		let content = advertising_item.querySelector(".advertising__text-hide");
+		// Needed if this is being fired after a resize
+		content.removeAttribute("aria-hidden");
+		// Height of content to show/hide
+		let heightOfContent = content.getBoundingClientRect().height;
+		// Set a CSS custom property with the height of content
+		advertising_item.style.setProperty("--containerHeight", `${heightOfContent}px`);
+		// Once height is read and set
+		setTimeout(e => {
+			content.setAttribute("aria-hidden", "true");
+		}, 0);
+	});
+}
+
+function wireUpTriggers() {
+	advertising_items.forEach(advertising_item => {
+		// Get each trigger element
+		let btn = advertising_item.querySelector(".advertising__hide-show-btn");
+		// Get content
+		let content = advertising_item.querySelector(".advertising__text-hide");
+		btn.addEventListener("click", () => {
+			content.setAttribute(
+				"aria-hidden",
+				content.getAttribute("aria-hidden") === "true" ? "false" : "true"
+			);
+		});
+	});
+}
